1e9bc0c37ef51afe663e225b2a2ae513b138a8f9
ArseneBrosy <arsene.brosy@divtec.ch>
2023-10-09 21:25:41 +0200

    define grid

--- js/script.js
+++ js/script.js
@@ -1,40 +1,42 @@
-// TEMPLATE
+// Flow Fields
 // by Arsène Brosy
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 
-//#region CONSTANTES
+//#region CONSTANTS
+const GRID_WIDTH = 10;
+const GRID_HEIGHT = 10;
 //#endregion
 
 //#region VARIABLES
-// souris
-let mouseX = 0;
-let mouseY = 0;
 //#endregion
 
 //#region FUNCTIONS
 function Distance(x1, y1, x2, y2) {
     return Math.sqrt(Math.abs(x1 - x2)**2 + Math.abs(y1 - y2)**2);
 }
+
+function degToVector(degree) {
+    return {
+        x: Math.cos(degree * (Math.PI / 180)),
+        y: Math.sin(degree * (Math.PI / 180))
+    }
+}
 //#endregion
 
-function loop() {
-    canvas.width = 1000;
-    canvas.height = 1000;
+setInterval(() => {
+    // resize canvas
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
 
     //#region DRAW
-    // clear
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "red";
-    ctx.strokeStyle = "red";
+    for (let y = 0; y < GRID_HEIGHT; y++) {
+        for (let x = 0; x < GRID_WIDTH; x++) {
+            let xPos = canvas.width * (x / (GRID_WIDTH - 1));
+            let yPos = canvas.height * (y / (GRID_WIDTH - 1));
+
+            ctx.fillRect(xPos - 2, yPos - 2, 4, 4);
+        }
+    }
     //#endregion
-    requestAnimationFrame(loop);
-}
-
-document.addEventListener("mousemove", (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-});
-
-// start game
-requestAnimationFrame(loop);
+}, 1);
