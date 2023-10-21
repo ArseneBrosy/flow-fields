59b719ef2adf6b1476249dcc8406f9c0f2c989c7
ArseneBrosy <arsene.brosy@divtec.ch>
2023-10-21 19:22:45 +0200

    delete useless past scripts

--- js/redirect.js
+++
@@ -1,8 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-
-const mode = urlParams.get("mode");
-
-const head= document.getElementsByTagName('head')[0];
-let newScript = document.createElement("script");
-newScript.src = `js/${mode}.js`;
-head.appendChild(newScript);
